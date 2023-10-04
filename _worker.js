50a286d91fa3d7d0056954e14fa0c593dc0bb155
wangshiw <51616825+wangshiw@users.noreply.github.com>
2023-10-04 16:36:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '2a0bb6d1-bc12-45e5-b5af-fdf3e425c457';
 
-const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+const proxyIPs = ['alejandracaiccedo.com', 'edtunnel-dgp.pages.dev', 'cdn-b100.xn--b6gac.eu.org', 'fbi.gov', 'icook.hk', 'russia.com', 'ip.sb', 'shopify.com', 'time.is', 'iplocation.io', 'icook.hk'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
